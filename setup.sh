c41d03e30b7637a9062e135c9702a0e68816cffb
samantha-michael <samandalorian@gmail.com>
2024-12-08 22:33:00 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,8 @@ mkdir -p ~/.streamlit/
 
 echo "\
 [server]\n\
-address = 0.0.0.0\n\
+port = \${PORT}\n\
 enableCORS = false\n\
 headless = true\n\
+address = \"0.0.0.0\"\n\
 " > ~/.streamlit/config.toml
